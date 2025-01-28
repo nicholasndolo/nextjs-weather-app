5452c125e38e53f5db946a64d081fb57b9ea97a5
Nicholas <nickndolo95@gmail.com>
2025-01-28 07:57:21 +0300

    create Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <main></main>
+    <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl-[16rem] m-auto">
+      <Navbar />
+    </main>
   );
 }
